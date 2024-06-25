07c73da2279b6094bfe35043853d4a606bc9ed8b
DavidCamposH <Dc040105@gmail.com>
2024-06-24 20:49:02 -0600

    Implementación de rutas
    Implementación de rutas para:
    1- Buscar, obtener y actualizar usuarios por id id.
    2- Obtención de todos los usuarios registrados.

--- routes/public.js
+++ routes/public.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {hashContrasena,compararContrasena} = require('../utils/hashContraseñas');
 const { generarAccessToken, generarRefreshToken ,verificarToken} = require('../utils/jwt');
 const USUARIO = require('../models/Usuario');
+const mongoose = require('mongoose');
 
 // Ruta para iniciar sesión y obtener tokens
 router.post('/login', async (req, res) => {
@@ -94,4 +95,89 @@ router.post('/refresh-token', async (req, res) => {
   }
 });
 
+// Buscar usuarios por id
+router.get('/usuario/:id', async (req, res) => {
+  const { id } = req.params;
+
+  // Validar que el id sea un ObjectId válido
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send('ID inválido');
+  }
+
+  try {
+    const usuario = await USUARIO.findById(id);
+
+    if (!usuario) {
+      return res.status(404).send('Usuario no encontrado');
+    }
+
+    return res.status(200).json(usuario);
+  } catch (error) {
+    console.error('Error al obtener el usuario:', error);
+    return res.status(500).send('Error en el servidor');
+  }
+});
+
+//Ruta para obtener todos los tipos de usuario
+router.get('/tiposUsuario', async (req, res) => {
+  try {
+    const tipos = await USUARIO.distinct('role');
+    return res.status(200).json(tipos);
+  } catch (error) {
+    console.error('Error al obtener los tipos de usuario:', error);
+    return res.status(500).send('Error en el servidor');
+  }
+});
+
+//ruta para obtener todos los usuarios registrados
+router.get('/usuarios', async (req, res) => {
+  try {
+    // Obtener todos los usuarios y seleccionar solo el campo 'correo'
+    
+    const correos = await USUARIO.find({}, 'correo');
+    return res.status(200).json(correos);
+  } catch (error) {
+    console.error('Error al obtener los usuarios:', error);
+    return res.status(500).send('Error en el servidor');
+  }
+
+});
+
+// Actualizar un usuario por ID
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { role, nombres, apellidos, correo, contrasena } = req.body;
+
+  // Validar que el id sea un ObjectId válido
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: 'ID inválido' });
+  }
+  try {
+    const usuario = await USUARIO.findById(id);
+
+    if (!usuario) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+
+    // Actualizar los campos del usuario
+    if (role) usuario.role = role;
+    if (nombres) usuario.nombres = nombres;
+    if (apellidos) usuario.apellidos = apellidos;
+    if (correo) usuario.correo = correo;
+    if (contrasena) {
+      const contrasenaHasheada = await hashContrasena(contrasena);
+      usuario.contrasena = contrasenaHasheada;
+    }
+
+    // Guardar los cambios en la base de datos
+    await usuario.save();
+
+    return res.status(200).json(usuario);
+  } catch (err) {
+    console.error('Error al actualizar el usuario:', err);
+    return res.status(500).json({ error: 'Error al actulizar los datos' });
+  }
+});
+
 module.exports = router;
+
